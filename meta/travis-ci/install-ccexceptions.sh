6529b836f93e83e09fe032e39c02a3710b0e127f
Marco Maggi <marco.maggi-ipsu@poste.it>
2018-05-11 12:36:20 +0200

    fixed script for travis

--- meta/travis-ci/install-ccexceptions.sh
+++ meta/travis-ci/install-ccexceptions.sh
@@ -7,7 +7,7 @@
 # We assume the script is run from the top directory of the build tree.
 
 PROGNAME=install-ccexceptions.sh
-VERSION=0.6.4-devel.3
+VERSION=0.7.2-beta.0
 STEM="ccexceptions-${VERSION}"
 ARCHIVE="${STEM}.tar.gz"
 SOURCE_URI="https://github.com/marcomaggi/ccexceptions/archive/v${VERSION}.tar.gz"
